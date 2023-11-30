09640aa5a6db4d2d8b1f9e7b4ddbd9cbf15aa02a
¨[Serghio07]¨ <[ticona465@gmail.com]>
2023-11-30 11:55:05 -0400

    componentes

---
+++ ejercicio12e/componentes/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Dashboard = () => {
+  return (
+    <div className='Dashboard '>
+      <h1> Dashboard</h1>
+
+    </div>
+    
+  )
+}
+export default Dashboard;
---
+++ ejercicio12e/organismos/informacion.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import  Header  from '../componentes/Header'
+import About from '../componentes/About'
+import  Home  from '../componentes/Home'
+import Dashboard from '../componentes/Dashboard'
+function Informacion() {
+    return (
+      <>
+      <Home />
+      <About/>
+      <Dashboard/>
+      <Header/>
+      </>
+    )
+  }
+  
+  export default Informacion
